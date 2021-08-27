9e768a0414bce3217ade81a13d695c28c1888316
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-08-27 11:27:18 +0300

    day1: parse input

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -1,6 +1,23 @@
 (ns day01.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input "resources\\input.txt")
+
+(defn str->int
+  "Converts a string to integer."
+  [s]
+  (Integer/parseInt s))
+
+(defn parse
+  "Splits the input string by \n and converts it into a list of numbers."
+  [s]
+  (map str->int (clojure.string/split s #"\n")))
+
+(def parsed-input (parse (slurp input)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
