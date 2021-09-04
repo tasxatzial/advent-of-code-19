db34c543a00171eace1900c78bec50d07dcd52ca
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-09-04 08:15:31 +0300

    day3: find common points of two vectors (specific directions RR, RU, UU)

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -34,8 +34,8 @@
   (let [wire-input (split-by-wire (slurp input-file))]
     (mapv parse-wire wire-input)))
 
-(def wire1-path (first parsed-input))
-(def wire2-path (second parsed-input))
+(def wire1-instructions (first parsed-input))
+(def wire2-instructions (second parsed-input))
 
 (defn next-location
   "Finds the next wire location given a [x y] location and an
@@ -47,22 +47,52 @@
     \U [x (+ y number)]
     \D [x (- y number)]))
 
-(defn wire-locations
-  "Returns a vector of locations based on the given wire path."
-  [wire-path]
-  (loop [locations [[0 0]]
-         wire-path wire-path]
-    (if (seq wire-path)
-      (let [last-loc (last locations)
-            instruction (first wire-path)
+(defn wire-path
+  "Returns a vector of locations based on the given wire instructions."
+  [wire-instructions]
+  (loop [locations [[\I [0 0]]]
+         wire-instructions wire-instructions]
+    (if (seq wire-instructions)
+      (let [last-loc (second (last locations))
+            [direction _ :as instruction] (first wire-instructions)
             new-loc (next-location last-loc instruction)]
-        (recur (conj locations new-loc) (rest wire-path)))
+        (recur (conj locations [direction new-loc]) (rest wire-instructions)))
       locations)))
 
 ; --------------------------
 ; problem 1
 
+; R R
+(defn common-points-horizontal
+  "Finds the common (integer) points of the two horizontal
+  vectors denoted as [[x1 y1] [x2 _]] and [[x3 y3] [x4 _]].
+  [x1 y1] and [x3 y3] are their starting points."
+  [[x1 y1] [x2 _] [x3 y3] [x4 _]]
+  (if (and (= y1 y3) (<= x1 x4) (<= x3 x2))
+    (for [x (range (max x1 x3) (inc (min x2 x4)))]
+      [x y1])
+    []))
+
+; R U
+(defn common-points-perpendicular
+  "Finds the common point of the two perpendicular
+   vectors denoted as [[x1 _] [x2 y2]] and [[_ y3] [x4 y4]].
+   [x1 _] and [_ y3] are their starting points."
+  [[x1 _] [x2 y2] [_ y3] [x4 y4]]
+  (if (and (<= x1 x4 x2) (<= y3 y2 y4))
+    [[x4 y2]]
+    []))
+
+(defn common-points-vertical
+  "Finds the common (integer) points of the two vertical
+  vectors denoted as [[x1 y1] [_ y2]] and [[x3 y3] [_ y4]].
+  [x1 y1] and [x3 y3] are their starting points."
+  [[x1 y1] [_ y2] [x3 y3] [_ y4]]
+  (if (and (= x1 x3) (<= y1 y4) (<= y3 y2))
+    (for [y (range (max y1 y3) (inc (min y2 y4)))]
+      [x1 y])
+    []))
 
 (defn -main
   []
-  (println (wire-locations wire1-path)))
+  (println (wire-path wire1-instructions)))
