a842b9b3037bc14e957f53049f3771d4bf3e332c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-09-09 08:13:46 +0300

    day4 problem 1: compute solution

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -1,7 +1,37 @@
 (ns day04.core
   (:gen-class))
 
+(defn int->digits
+  "Converts an integer to a sequence of its digits."
+  [s]
+  (map #(Character/digit ^char % 10)
+       (str s)))
+
+(def low-num 172851)
+(def high-num 675869)
+
+; --------------------------
+; problem 1
+
+(defn valid-password?
+  [digits]
+  (and (apply <= digits) (not (apply < digits))))
+
+(defn count-valid-passwords1
+  "Counts the valid passwords contained in [low-num high-num]"
+  []
+  (->> (range low-num (inc high-num))
+       (map int->digits)
+       (filter valid-password?)
+       count))
+
+; ---------------------------------------
+; results
+
+(defn day04-1
+  []
+  (count-valid-passwords1))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (day04-1)))
