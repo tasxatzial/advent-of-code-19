1952e0e02b50d4e8a716fbc549b86efd80d9da83
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-09-09 08:47:49 +0300

    day4 problem 2: compute solution

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -4,8 +4,7 @@
 (defn int->digits
   "Converts an integer to a sequence of its digits."
   [s]
-  (map #(Character/digit ^char % 10)
-       (str s)))
+  (map #(Character/digit ^char % 10) (str s)))
 
 (def low-num 172851)
 (def high-num 675869)
@@ -13,25 +12,44 @@
 ; --------------------------
 ; problem 1
 
-(defn valid-password?
+(defn valid-password1?
+  "Checks whether a digits list represents a valid password.
+  Ignores the range rule."
   [digits]
-  (and (apply <= digits) (not (apply < digits))))
+  (and (apply <= digits)
+       (not (apply < digits))))
 
-(defn count-valid-passwords1
+(defn count-valid-passwords
   "Counts the valid passwords contained in [low-num high-num]"
-  []
+  [check-password-fn]
   (->> (range low-num (inc high-num))
        (map int->digits)
-       (filter valid-password?)
+       (filter check-password-fn)
        count))
+
+; --------------------------
+; problem 2
+
+(defn valid-password2?
+  "Checks whether a digits list represents a valid password.
+  Ignores the range rule."
+  [digits]
+  (and (apply <= digits)
+       (some #(= 2 (count %))
+             (partition-by identity digits))))
 
 ; ---------------------------------------
 ; results
 
 (defn day04-1
   []
-  (count-valid-passwords1))
+  (count-valid-passwords valid-password1?))
+
+(defn day04-2
+  []
+  (count-valid-passwords valid-password2?))
 
 (defn -main
   []
-  (println (day04-1)))
+  (println (day04-1))
+  (println (day04-2)))
