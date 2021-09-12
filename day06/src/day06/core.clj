14d2bcedd17e3cbe449f07a2f33115d185f9e496
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-09-12 11:17:39 +0300

    day6: parse input

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -1,7 +1,23 @@
 (ns day06.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-input
+  "Parses the input and returns a map of orbits. A key
+  represents the object that orbits the corresponding value."
+  [s]
+  (let [lines (clojure.string/split s #"\n")]
+    (reduce (fn [result line]
+              (let [[obj1 obj2] (clojure.string/split line #"\)")]
+                (conj result [(keyword obj2) (keyword obj1)])))
+            {} lines)))
+
+(def orbits (parse-input (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println orbits))
