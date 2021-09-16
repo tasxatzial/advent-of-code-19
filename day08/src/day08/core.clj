648de6bcaf411c98ba0ec4de899cb47f81f2b1a2
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-09-16 09:47:11 +0300

    day8 problem 1: compute solution

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -22,6 +22,37 @@
 
 (def layers (parse-input-file))
 
+; --------------------------
+; problem 1
+
+(defn count-layer-num
+  "Counts the number of nums in the given layer."
+  [layer num]
+  (reduce (fn [result row]
+            (+ result (count (filter #(= num %) row))))
+          0 layer))
+
+(defn find-min-zeros-layer
+  "Finds the layer with the min number of zeros."
+  []
+  (let [layer-zeros (mapv #(count-layer-num % 0) layers)
+        min-zeros (apply min layer-zeros)]
+    (loop [index 0]
+      (let [zero-count (get layer-zeros index)]
+        (if (= zero-count min-zeros)
+          (nth layers index)
+          (recur (inc index)))))))
+
+; ---------------------------------------
+; results
+
+(defn day08-1
+  []
+  (let [min-zeros-layer (find-min-zeros-layer)
+        ones-count (count-layer-num min-zeros-layer 1)
+        twos-count (count-layer-num min-zeros-layer 2)]
+    (* ones-count twos-count)))
+
 (defn -main
   []
-  (println layers))
+  (println (day08-1)))
