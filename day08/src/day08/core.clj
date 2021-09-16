6ea565382a70c0bb2e9596b46ac88cd163722fad
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-09-16 08:50:18 +0300

    day8: parse input

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -1,7 +1,27 @@
 (ns day08.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(def wide 25)
+(def tall 6)
+
+(defn parse-input-file
+  "Parses the input file and creates the layers of the image.
+  The returned structure is a list of layers. Each layer
+  consists of 'tall' number of lists, each list contains
+  'wide' total numbers."
+  []
+  (->> (slurp input-file)
+       (map #(Character/digit ^char % 10))
+       (partition wide)
+       (partition tall)))
+
+(def layers (parse-input-file))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println layers))
