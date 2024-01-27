3ada2ec19c9dc15c867b2ad835bedfa8bcc9c293
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-01-28 00:16:50 +0200

    day4: refactoring, update docstrings

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -1,37 +1,41 @@
 (ns day04.core
   (:gen-class))
 
+; --------------------------
+; common
+
 (defn int->digits
   "Converts an integer to a sequence of its digits."
   [s]
   (map #(Character/digit ^char % 10) (str s)))
 
-(def low-num 172851)
-(def high-num 675869)
+(def low-pass 172851)
+(def high-pass 675869)
+
+(defn count-valid-passwords
+  "Counts the valid passwords contained in [low-pass high-pass].
+  Accepts a predicate function."
+  [valid-password?]
+  (->> (range low-pass (inc high-pass))
+       (map int->digits)
+       (filter valid-password?)
+       count))
 
 ; --------------------------
 ; problem 1
 
-(defn valid-password1?
-  "Checks whether a digits list represents a valid password.
+(defn p1_valid-password?
+  "Checks whether the given list of digits represents a valid password.
   Ignores the range rule."
   [digits]
   (and (apply <= digits)
        (not (apply < digits))))
 
-(defn count-valid-passwords
-  "Counts the valid passwords contained in [low-num high-num]"
-  [check-password-fn]
-  (->> (range low-num (inc high-num))
-       (map int->digits)
-       (filter check-password-fn)
-       count))
-
 ; --------------------------
 ; problem 2
 
-(defn valid-password2?
-  "Checks whether a digits list represents a valid password.
+(defn p2_valid-password?
+  "Checks whether the given list of digits represents a valid password.
   Ignores the range rule."
   [digits]
   (and (apply <= digits)
@@ -43,11 +47,11 @@
 
 (defn day04-1
   []
-  (count-valid-passwords valid-password1?))
+  (count-valid-passwords p1_valid-password?))
 
 (defn day04-2
   []
-  (count-valid-passwords valid-password2?))
+  (count-valid-passwords p2_valid-password?))
 
 (defn -main
   []
