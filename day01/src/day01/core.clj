312b7d7b9cae08eaceb52f5743fc2f4db56ff165
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-01-27 23:34:14 +0200

    day1: refactoring, update docstrings

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -4,56 +4,53 @@
 ; --------------------------
 ; common
 
-(def input "resources\\input.txt")
+(def input-file "resources\\input.txt")
 
-(defn str->int
-  "Converts a string to integer."
-  [s]
-  (Integer/parseInt s))
+(defn parse-file
+  "Reads and parses the input file into a vector of numbers."
+  []
+  (->> input-file
+       slurp
+       clojure.string/split-lines
+       (map #(Integer/parseInt %))))
 
-(defn parse
-  "Splits the input string by \n and converts it into a list of numbers."
-  [s]
-  (map str->int (clojure.string/split s #"\n")))
-
-(def parsed-input (parse (slurp input)))
+(def memoized-input-file->masses (memoize parse-file))
 
 ; --------------------------
 ; problem 1
 
-(defn fuel
-  "Returns the required fuel for a given module"
-  [module]
-  (Math/round (- (Math/floor (/ module 3)) 2)))
+(defn p1_calc-fuel
+  "Returns the required fuel for a given mass."
+  [mass]
+  (Math/round (- (Math/floor (/ mass 3)) 2)))
 
 ; --------------------------
 ; problem 2
 
-(defn fuel-list
-  "Returns the fuel list for a given module"
-  [module]
-  (loop [fuels []
-         module module]
-    (let [f (fuel module)]
-      (if (pos? f)
-        (recur (conj fuels f) f)
-        fuels))))
-
-(defn total-fuel
-  "Returns the total fuel for a given module"
-  [module]
-  (apply + (fuel-list module)))
+(defn p2_calc-fuel
+  "Returns the required fuel for a given mass."
+  [mass]
+  (loop [total-fuel 0
+         mass mass]
+    (let [fuel (p1_calc-fuel mass)]
+      (if (pos? fuel)
+        (recur (+ total-fuel fuel) fuel)
+        total-fuel))))
 
 ; ---------------------------------------
 ; results
 
 (defn day01-1
   []
-  (apply + (map fuel parsed-input)))
+  (->> (memoized-input-file->masses)
+       (map p1_calc-fuel)
+       (reduce +)))
 
 (defn day01-2
   []
-  (apply + (map total-fuel parsed-input)))
+  (->> (memoized-input-file->masses)
+       (map p2_calc-fuel)
+       (reduce +)))
 
 (defn -main
   []
