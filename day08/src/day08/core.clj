192b846b9c59aa3373272c09adb924617b23feb6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-01-28 19:53:00 +0200

    day8: re-write

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -6,68 +6,87 @@
 
 (def input-file "resources\\input.txt")
 
-(def wide 25)
-(def tall 6)
+(def image-width 25)
+(def image-height 6)
 
-(defn parse-input-file
-  "Parses the input file and creates the layers of the image.
-  The returned structure is a list of layers. Each layer
-  consists of 'tall' number of lists, each list contains
-  'wide' total numbers."
+(defn parse-file
+  "Reads and parses the input file into a vector of vectors.
+  Each vector corresponds to an image layer and contains image-height
+  number of vectors each containing image-width integers."
   []
-  (->> (slurp input-file)
+  (->> input-file
+       slurp
        (map #(Character/digit ^char % 10))
-       (partition (* wide tall))))
+       (partition (* image-width image-height))
+       (map #(partition image-width %))
+       (map #(map vec %))
+       (mapv vec)))
 
-(def layers (parse-input-file))
+(def memoized-input-file->image (memoize parse-file))
 
 ; --------------------------
 ; problem 1
 
-(defn count-layer-num
-  "Counts the number of nums in the given layer."
-  [layer num]
-  (count (filter #(= num %) layer)))
+(defn count-value
+  "Returns the val count in the given layer."
+  [layer val]
+  (->> layer
+       (map #(filter #{val} %))
+       (map count)
+       (reduce +)))
 
 (defn find-min-zeros-layer
   "Finds the layer with the min number of zeros."
   []
-  (let [layer-zeros (mapv #(count-layer-num % 0) layers)
-        min-zeros (apply min layer-zeros)]
-    (loop [index 0]
-      (let [zero-count (get layer-zeros index)]
-        (if (= zero-count min-zeros)
-          (nth layers index)
-          (recur (inc index)))))))
+  (loop [[layer & rest-layers] (memoized-input-file->image)
+         min-layer nil
+         min-zeros (* image-width image-height)]
+    (if layer
+      (let [zero-count (count-value layer 0)]
+        (if (< zero-count min-zeros)
+          (recur rest-layers layer zero-count)
+          (recur rest-layers min-layer min-zeros)))
+      min-layer)))
 
 ; --------------------------
 ; problem 2
 
+(defn get-pixel-val
+  "Returns the value of the pixel at the given layer,row,col."
+  [layer row col]
+  (get-in layer [row col]))
+
+(defn get-pixel-vals
+  "Returns a sequence of the pixel values at the given row,col."
+  [row col]
+  (->> (memoized-input-file->image)
+       (map #(get-pixel-val % row col))))
+
 (defn find-first-visible
-  "Finds the first visible pixel in position index."
-  [index]
-  (let [i-pixels (map #(nth % index) layers)]
-    (some #(and (not= 2 %) %) i-pixels)))
+  "Finds the value of the first visible pixel at the given row,col."
+  [row col]
+  (->> (get-pixel-vals row col)
+       (some #(and (not= 2 %) %))))
 
 (defn decode-image
-  "Decodes the image by finding the first visible pixel in every position."
+  "Returns the decoded image as a string."
   []
-  (let [layer-pixel-count (count (first layers))]
-    (loop [result []
-           index 0]
-      (if (= layer-pixel-count index)
-        (partition wide result)
-        (recur (conj result (find-first-visible index))
-               (inc index))))))
+  (let [decoded-pixels (for [row (range image-height)
+                             col (range image-width)]
+                         (find-first-visible row col))]
+    (->> decoded-pixels
+         (partition image-width)
+         (map #(apply str %))
+         (clojure.string/join "\n"))))
 
 ; ---------------------------------------
 ; results
 
 (defn day08-1
   []
-  (let [min-zeros-layer (find-min-zeros-layer)
-        ones-count (count-layer-num min-zeros-layer 1)
-        twos-count (count-layer-num min-zeros-layer 2)]
+  (let [min-layer (find-min-zeros-layer)
+        ones-count (count-value min-layer 1)
+        twos-count (count-value min-layer 2)]
     (* ones-count twos-count)))
 
 (defn day08-2
